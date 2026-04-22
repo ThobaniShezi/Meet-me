e883b58c4f9d6ade96d48dbc8387f05ca2c77e92
ThobaniShezi <21301095@dut4life.ac.za>
2026-04-22 09:14:45 +0200

    Updated portfolio with apps, contact section and privacy policy

--- script.js
+++ script.js
@@ -1,90 +1,75 @@
-$(document).ready(function () {
-  $(window).scroll(function () {
-    //  sticky navbar on scroll script  //
-    if (this.scrollY > 20) {
-      $(".navbar").addClass("sticky");
-    } else {
-      $(".navbar").removeClass("sticky");
-    }
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if (this.scrollY > 20) {
+            $(".navbar").addClass("sticky");
+        } else {
+            $(".navbar").removeClass("sticky");
+        }
 
-    //  scroll-up button show/hide script  //
-    if (this.scrollY > 500) {
-      $(".scroll-up-btn").addClass("show");
-    } else {
-      $(".scroll-up-btn").removeClass("show");
-    }
-  });
+        if (this.scrollY > 500) {
+            $(".scroll-up-btn").addClass("show");
+        } else {
+            $(".scroll-up-btn").removeClass("show");
+        }
+    });
 
-  //  slide-up script  //
+    $(".scroll-up-btn").click(function() {
+        $("html").animate({ scrollTop: 0 });
+        $("html").css("scrollBehavior", "auto");
+    });
 
-  $(".scroll-up-btn").click(function () {
-    $("html").animate({ scrollTop: 0 });
-    //  removing smooth scroll on slide-up button click  //
-    $("html").css("scrollBehavior", "auto");
-  });
+    $(".navbar .menu li a").click(function() {
+        $("html").css("scrollBehavior", "smooth");
+    });
 
-  $(".navbar .menu li a").click(function () {
-    //  Smooth scroll on Menu Items click  //
+    $(".menu-btn").click(function() {
+        $(".navbar .menu").toggleClass("active");
+        $(".menu-btn i").toggleClass("active");
+    });
 
-    $("html").css("scrollBehavior", "smooth");
-  });
+    new Typed(".typing", {
+        strings: [
+            "App Developer",
+            "IT Support Specialist",
+            "Software Developer",
+            "Tech Enthusiast"
+        ],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
 
-  //  Toggle Navbar  //
+    new Typed(".typing-2", {
+        strings: [
+            "App Developer",
+            "IT Support Specialist",
+            "Software Developer",
+            "Problem Solver"
+        ],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
 
-  $(".menu-btn").click(function () {
-    $(".navbar .menu").toggleClass("active");
-    $(".menu-btn i").toggleClass("active");
-  });
-
-  //  Typing Text Animation  //
-
-  var typed = new Typed(".typing", {
-    strings: [
-      "Fullstack Developer",
-      "Software Developer",
-      "Python Developer",
-      "Java Developer",
-      "Remote Help desk"
-    ],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-  });
-
-  var typed = new Typed(".typing-2", {
-    strings: [
-      "Fullstack Developer",
-      "Software Developer",
-      "Python Developer",
-      "Founder",
-      "Author"
-    ],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-  });
-
-  //  Owl Carousel  //
-
-  $(".carousel").owlCarousel({
-    margin: 20,
-    loop: true,
-    autoplay: true,
-    autoplayTimeOut: 2000,
-    autoplayHoverPause: true,
-    responsive: {
-      0: {
-        items: 1,
-        nav: false
-      },
-      600: {
-        items: 2,
-        nav: false
-      },
-      1000: {
-        items: 3,
-        nav: false
-      }
-    }
-  });
+    $(".carousel").owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
 });
